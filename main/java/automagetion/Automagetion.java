a3ffe6f8ee70997832257fd990a9e35363565dd3
Benjamin Ranson <benr444@bellsouth.net>
2018-04-22 19:17:51 -0400

    Initial Commit
    File/Folder structure

---
+++ main/java/automagetion/Automagetion.java
@@ -0,0 +1,20 @@
+package automagetion;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Automagetion.MODID, version = Automagetion.VERSION)
+public class Automagetion
+{
+    public static final String MODID = "automagetion";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
