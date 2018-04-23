c5e40662c74fe029644013b07ec2e1f392a6bb59
bakugantj <bakugantj@gmail.com>
2018-04-22 22:02:21 -0400

    Implemented Item Textures
    -TJ

--- main/java/automagetion/item/ItemRenderRegister.java
+++ main/java/automagetion/item/ItemRenderRegister.java
@@ -9,23 +9,31 @@ public class ItemRenderRegister
 {
 	public static void registerItemRenderer() 
 	{
-		Minecraft.getMinecraft()
-		.getRenderItem()
-		.getItemModelMesher()
-		.register(BasicItem.animo, 
-				0, 
-				new ModelResourceLocation(
-						modid + 
-						":" + 
-						BasicItem.animo.getUnlocalizedName()
-				.substring(5),
-				"inventory"));
+		reg(BasicItem.chantRootFibers);
+		reg(BasicItem.reedAssembly);
+		reg(BasicItem.songPaper);
+		reg(BasicItem.animo);
+		reg(BasicItem.bellum);
+		reg(BasicItem.caelum);
+		reg(BasicItem.calor);
+		reg(BasicItem.cantio);
+		reg(BasicItem.creo);
+		reg(BasicItem.evorsio);
+		reg(BasicItem.funus);
+		reg(BasicItem.imperium);
+		reg(BasicItem.inanis);
+		reg(BasicItem.infernum);
+		reg(BasicItem.irrigus);
+		reg(BasicItem.mundus);
+		reg(BasicItem.natura);
+		reg(BasicItem.pax);
+		reg(BasicItem.sopio);
 	}
 
 	public static String modid = Automagetion.MODID;
 
 	public static void reg(Item item) 
 	{
-	    //Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
 	}
 }
